d01d6cadd7ab0780d8a22a953630f2caacdf20b5
Tim Skaggs <me@timskaggs.com>
2017-06-28 18:12:38 -0600

    User List, User Form, update CSS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 var m = require("mithril")
 
-var MyComponent = require("./components/mycomponent")
+// var MyComponent = require("./components/mycomponent")
+//
+// m.mount(document.body, MyComponent)
 
-m.mount(document.body, MyComponent)
+var UserList = require("./views/UserList")
+var UserForm = require("./views/UserForm")
+var Layout = require("./views/Layout")
+
+// m.mount(document.body, UserList)
+m.route(document.body, "/list", {
+  "/list": {
+    render: function() {
+      return m(Layout, m(UserList))
+    }
+  },
+  "/edit/:id": {
+    render: function(vnode) {
+      return m(Layout, m(UserForm, vnode.attrs))
+    }
+  },
+})
